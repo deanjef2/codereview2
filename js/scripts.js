37a084bc5de6cdab60800632d9734e095756ee27
Jeff Dean <deanjef2@hotmail.com>
2016-03-06 12:51:50 -0800

    completes website

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,31 @@ $(document).ready(function() {
     var swimming = $("select#swimming").val();
     var age = $("select#age").val();
 
+    $(".destination").hide();
+
+    if (hot === "hotYes") {
+      $("#Hawaii").show();
+    } else if (hot === "hotNo") {
+      $("#Antarctica").show ();
+    }
+
+    if (age === "ageYes") {
+      $("#Hawaii").show();
+    } else if (age === "ageNo") {
+      $("#Rome").show ();
+      }
+
     if (age === "ageYes" && swimming === "swimmingNo") {
       $("#Rome").show();
-    } else if (age === "ageNo" & swimming === "swimmingYes")
+    } else if (age === "ageNo" & swimming === "swimmingYes") {
       $("#Antarctica").show();
     }
 
-    if (animals === "animalsYes" && hot === "hotYes")
+    if (animals === "animalsYes" && hot === "hotYes") {
       $("#Hawaii").show();
-    } else if (animals === "animalsNo" && hot === "hotNo")
+    } else if (animals === "animalsNo" && hot === "hotNo") {
       $("#Rome").show();
+    }
     event.preventDefault();
   });
 });
